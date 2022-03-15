7ee541886c7f099d810c862d5772839862ac2d5c
AOTAzx <94093902+AOTAzx@users.noreply.github.com>
2022-03-15 23:55:35 +0700

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,10 @@
+@echo off
+pip install pyarmor
+pip install pymongo
+pip install discord
+pip install aiohttp
+pip install pypresence
+pip install tasksio
+pip install colorama
+pip install requests
+pause
